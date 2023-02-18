bae34b7ac91c9749523ba36ee7e64f79bcc1b264
David Adenuga <adenugadavid8@gmail.com>
2023-02-18 23:33:26 +0100

    final updates

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["David", "Adenuga's", "Portfolio"],
       typeSpeed: 100,
       loop: true,
     });
